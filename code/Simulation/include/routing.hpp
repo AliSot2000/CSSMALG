ae88b05e836c6893ae0bcef2010a1e60a7f78699
AliSot2000 <alisot200@gmail.com>
2022-11-22 23:34:39 +0100

    + Docstrings + const ref where possible (suggested)

--- code/Simulation/include/routing.hpp
+++ code/Simulation/include/routing.hpp
@@ -50,6 +50,23 @@
 
 typedef std::map<std::string, std::map<std::string, std::string>> SPT;
 
-SPT calculateShortestPathTree(const world_t* world, const std::vector<StreetTypes> include);
+/*
+ * Calculates the shortest path tree for the given world.
+ *
+ * @param world The world to calculate the shortest path tree for.
+ * @param include The types of streets to include in the calculation.
+ *
+ * @return The shortest path tree.
+ */
+SPT calculateShortestPathTree(const world_t* world, const std::vector<StreetTypes>& include);
 
-Path retrievePath(SPT& spt, const std::string start, const std::string end);
+/*
+ * Retrieves the path from start to end.
+ *
+ * @param spt The shortest path tree.
+ * @param start The start of the path, string_id from the web_interface.
+ * @param end The end of the path, string_id from the web_interface.
+ *
+ * @return The path from start to end.
+ */
+Path retrievePath(SPT& spt, const std::string &start, const std::string &end);
