b3690ae19378246e58d47cfd9ef29e8506960b12
nilsegger <nilsxegger@gmail.com>
2022-11-29 14:35:25 +0100

    added comments to actor.hpp

--- code/Simulation/include/actors.hpp
+++ code/Simulation/include/actors.hpp
@@ -9,17 +9,17 @@
 typedef std::queue<std::string> Path;
 
 enum ActorTypes {
+	None,
 	Bike,
 	Car
 };
 
 typedef struct Actor {
-	ActorTypes type = ActorTypes::Car;
+	ActorTypes type = ActorTypes::None;
 
-    // Position of the actor.
-	float distanceToCrossing = 50.0f;
-    int distanceToRight = 0;
-    float length = 4.5f; //
+	float distanceToCrossing = 0.0f; // Distance from street end in meters
+    int distanceToRight = 0; // Distance to right curb in meters, always a factor of LANE_WIDTH defined in update.hpp
+    float length = 0.f; // Length of vehicle in meters
 
     // Velocity
     float current_velocity = 0.0f; // m/s
@@ -34,9 +34,10 @@ typedef struct Actor {
 
     float insertAfter = 0.0f; // After how many seconds the actor should try to be inserted at the crossing
 
+	Path path;
+
 	// Only used for visualization
 	std::string id = "empty";
-	Path path;
 	bool outputFlag = false; // this flag is needed for exporter to know if non-active actors status has been outputted once
 } actor_t;
 
@@ -47,36 +48,34 @@ enum StreetTypes {
 };
 
 typedef struct Street {
-	std::string start = "";
-	std::string end = "";
+	std::string start = ""; // ID of start crossing
+	std::string end = ""; // ID of end crossing
 
-	struct Street* opposite = nullptr;
 	StreetTypes type = StreetTypes::Both;
-	size_t width = 2;
-	float length = 100.0f;
-    float speedlimit = 50.0f; // km/h
+	size_t width = 0; // Width of street in meters, should be a factor of LANE_WIDTH defined in update.hpp
+	float length = 0.0f; // Length of street in meters
+    float speedlimit = 0.0f; // Speed Limit for vehicles in km/h
 
-	// Ordered by distance to end, must be reordered when actors change position
-	// Furthermore, when vehicles swap position, their position to the left of the road side must be swapped aswell
+	// Ordered list of all actors on street. Ordered by ascending Actor::distanceToCrossing 
 	std::vector<Actor*> traffic;
 
 	// These values are not used by the simulation itself, just for the visulisation later
-	// start and end position
 	std::string id;
 } street_t;
 
 typedef struct Crossing {
-	std::string id;
-	std::vector<Street*> inbound;
-	std::map<std::string, Street*> outbound;
-	float greenPhaseDuration = 5.0f;
+	std::string id; // ID of crossing, used for routing and visualization
+	std::vector<Street*> inbound; // All streets which end in this crossing
+	std::map<std::string, Street*> outbound; // All streets which exit this crossing, leading to next crossing defined by its string id. <ID, Street*>
+	float greenPhaseDuration = 5.0f; // Defines how long a traffic light stays green in seconds
 	float currentPhase = 5.0f;
-	int32_t green = 0;
+	int32_t green = 0; // Index of inbound street which is currently green
 
-	std::vector<Actor*> waitingToBeInserted;
-	std::vector<std::pair<Actor*, Street*>> arrivedFrom;
+	std::vector<Actor*> waitingToBeInserted; // All actors which have not been inserted into simulation yet. Either waiting for space or until its insertAfter has expired
+	std::vector<std::pair<Actor*, Street*>> arrivedFrom; // Contains all actors which have arrived at its destination, including from which street they came.
 } crossing_t;
 
+// Container for all simulation data, best practice is  to never use std::vector::push_back, so that all data is aligend in memory.
 typedef struct World {
 	std::vector<Crossing> crossings;
 	std::vector<Street> streets;
