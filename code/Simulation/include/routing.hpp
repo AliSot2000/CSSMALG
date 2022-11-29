eee8e5503ab3395e19877aca2e9eca8b61988b1e
nilsegger <nilsxegger@gmail.com>
2022-11-29 15:07:23 +0100

    refactored routing.*pp

--- code/Simulation/include/routing.hpp
+++ code/Simulation/include/routing.hpp
@@ -1,57 +1,17 @@
 #pragma once
 
 #include <map>
+
 #include "actors.hpp"
 
 /*
 	Inspired by https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm#Path_reconstruction
-
-	Demo taken from wikipedia, results in correct path being chosen.
-
-	world_t world;
-
-	world.crossings = std::vector<Crossing>(4);
-	world.streets = std::vector<Street>(5);
-
-	world.crossings[0].id = 0;
-	world.crossings[1].id = 1;
-	world.crossings[2].id = 2;
-	world.crossings[3].id = 3;
-
-	world.streets[0].start = 0;
-	world.streets[0].end = 2;
-	world.streets[0].length = -2.0f;
-
-	world.streets[1].start = 2;
-	world.streets[1].end = 3;
-	world.streets[1].length = 2.0f;
-
-	world.streets[2].start = 3;
-	world.streets[2].end = 1;
-	world.streets[2].length = -1.0f;
-
-	world.streets[3].start = 1;
-	world.streets[3].end = 0;
-	world.streets[3].length = 4.0f;
-
-	world.streets[4].start = 1;
-	world.streets[4].end = 2;
-	world.streets[4].length = 3.0f;
-
-	SPT spt = calculateShortestPathTree(&world);
-	std::queue<int32_t> path = retrievePath(spt, 3, 2);
-
-	while (!path.empty()) {
-		int32_t step = path.front();
-		std::cout << "Step: " << step << std::endl;
-		path.pop();
-	}
 */
 
-typedef std::map<std::string, std::map<std::string, std::string>> SPT;
+typedef std::map<std::string, std::map<std::string, std::string>> SPT; // Shortest Path Tree C++ struct
 
 /*
- * Calculates the shortest path tree for the given world.
+ * Calculates the shortest path tree for the given world using Floyd-Warshall
  *
  * @param world The world to calculate the shortest path tree for.
  * @param include The types of streets to include in the calculation.
--- code/Simulation/src/routing.cpp
+++ code/Simulation/src/routing.cpp
@@ -4,11 +4,6 @@
 #include <iostream>
 #include <algorithm>
 
-// Idea: If a road has multiple turning lanes, split a crossing into sets of identical turn options and split the single
-// Crossing vertex into multiple vertecies representing the crossing with each new vertex only containing roads with
-// identical turning sets.
-
-// Compute Floyd-Warshal on entire graph to find the shortest path from a to b.
 SPT calculateShortestPathTree(const world_t* world, const std::vector<StreetTypes>& include) {
 	const size_t n = world->crossings.size();
 
@@ -20,6 +15,7 @@ SPT calculateShortestPathTree(const world_t* world, const std::vector<StreetType
 		minimumDistance[crossing.id] = std::map<std::string, float>();
 	}
 
+	// Insert all beginning vertices
 	for (const auto& street : world->streets) {
 		if (std::find(include.begin(), include.end(), street.type) != include.end()) {
 			minimumDistance[street.start][street.end] = street.length;
@@ -27,21 +23,26 @@ SPT calculateShortestPathTree(const world_t* world, const std::vector<StreetType
 		}
 	}
 
+	// Self to self should always be the shortest path
 	for (const auto& crossing : world->crossings) {
 		minimumDistance[crossing.id][crossing.id] = 0;
 		spt[crossing.id][crossing.id] = crossing.id;
 	}
 
+	// Floyd-Warshall Implementation
 	for (int32_t k = 0; k < n; k++) {
 		std::string ks = world->crossings[k].id;
+
 		for (int32_t i = 0; i < n; i++) {
 			std::string is = world->crossings[i].id;
+
 			for (int32_t j = 0; j < n; j++) {
 				std::string js = world->crossings[j].id;
 
+				// Check if there even is a path
 				bool hasEdge = spt[is].contains(ks) && spt[ks].contains(js);
-                // Check if the two edges is-ks, ks-js are valid.
-                // If the nodes is, js are not connected, connect them, else update the value if the connections is shorter.
+
+				// Check if other path would be shorter
 				if (hasEdge && (!spt[is].contains(js) || minimumDistance[is][js] > minimumDistance[is][ks] + minimumDistance[ks][js])) {
 					minimumDistance[is][js] = minimumDistance[is][ks] + minimumDistance[ks][js];
 					spt[is][js] = spt[is][ks];
@@ -54,17 +55,22 @@ SPT calculateShortestPathTree(const world_t* world, const std::vector<StreetType
 }
 
 Path retrievePath(SPT& spt, const std::string &start, const std::string &end) {
+
 	if (!spt[start].contains(end)) {
+		// No path exists
 		return Path();
 	}
 
 	Path p;
-	// p.push(start);
 
+	// Start is not included, otherwise p.push(start)
+
+	// Reverse path lookup
 	std::string u = start;
 	while (u != end) {
 		u = spt[u][end];
 		p.push(u);
 	}
+
 	return p;
 }
