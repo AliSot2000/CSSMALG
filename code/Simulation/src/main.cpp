4f51b51a0fb2f42ba37041eb3df77d4d2e48d4cf
AliSot2000 <alisot200@gmail.com>
2022-12-02 13:09:19 +0100

    + Moved everything to the utils file so i can reuse the functions

--- code/Simulation/src/main.cpp
+++ code/Simulation/src/main.cpp
@@ -11,84 +11,6 @@
 #include "update.hpp"
 #include "io.hpp"
 
-int randint(int min, int max) {
-	return std::rand() % (max - min + 1) + min;
-}
-
-void choseRandomPath(world_t& world, SPT& spt, std::string& start, std::string& end) {
-	if (world.crossings.size() == 0) {
-		std::cerr << "There are no crossings." << std::endl;
-		return;
-	}
-	int len = (int)world.crossings.size() - 1;
-	SPT::iterator startIter = spt.begin();
-	std::advance(startIter, randint(0, len));
-	int noFinityLoop = 0;
-	while (startIter->second.size() < 2 && noFinityLoop <= world.crossings.size()) {
-		startIter = spt.begin();
-		std::advance(startIter, randint(0, len));
-		noFinityLoop++;
-	}
-
-	if (noFinityLoop > world.crossings.size()) {
-		std::cerr << "There exists no paths. Meaning one can only go from a intersection to the intersection itself." << std::endl;
-		return;
-	}
-
-	auto endIter = startIter->second.begin();
-	std::advance(endIter, randint(0, (int)startIter->second.size() - 1));
-	
-	if (endIter->first == startIter->first) {
-		endIter = startIter->second.begin();
-		if (endIter->first == startIter->first) 
-			endIter = std::next(endIter);
-	}
-
-	start = startIter->first;
-	end = endIter->first;
-}
-
-void createRandomActors(world_t& world, SPT& spt, const ActorTypes type, const int minSpeed, const int maxSpeed,
-                        const std::vector<Actor>::iterator& start, const std::vector<Actor>::iterator& end, const float length, const int max_start_time) {
-    for (std::vector<Actor>::iterator iter = start; iter != end; iter++) {
-        Actor actor = {
-                .type = type,
-                .distanceToCrossing = 0.0f,
-                .distanceToRight = 0,
-                .length = length,
-                .max_velocity = static_cast<float>(randint(minSpeed, maxSpeed)) * 0.277778f, // 30km/h to 80km/h
-                //.width = 1.5f,
-                .insertAfter = static_cast<float>(randint(0, max_start_time)),
-                .id = std::to_string(std::rand())
-        };
-
-		
-		std::string start_id;
-		std::string end_id;
-		choseRandomPath(world, spt, start_id, end_id);
-		actor.path = retrievePath(spt, start_id, end_id);
-
-		for (auto& crossing : world.crossings) {
-			if (crossing.id == start_id) {
-				crossing.waitingToBeInserted.push_back(&(*iter));
-				break;
-			}
-		}
-		*iter = actor;
-	}
-}
-
-std::chrono::high_resolution_clock::time_point start_time;
-
-void startMeasureTime(const std::string &task) {
-	std::cout << "Starting task: " << task << std::endl;
-    start_time = std::chrono::high_resolution_clock::now();
-}
-
-void stopMeasureTime() {
-	std::cout << "Last task took " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() / 1000.f << " seconds.\n\n" << std::endl;
-}
-
 int main(int argc, char* argv[]) {
 	if (argc < 6) {
 		std::cerr << "Usage CSSMALG <map-in> <sim-out> <n-random-cars> <n-random-bikes> <runtime> <runtime-step-time> optional <agents> <stupid_crossing>" << std::endl;
@@ -127,8 +49,12 @@ int main(int argc, char* argv[]) {
 	startMeasureTime("calculating shortest path tree with floyd warshall");
 	SPT carsSPT = calculateShortestPathTree(&world, { StreetTypes::Both, StreetTypes::OnlyCar});
 	SPT bikeSPT = calculateShortestPathTree(&world, { StreetTypes::Both, StreetTypes::OnlyBike });
-	stopMeasureTime();
 
+    nlohmann::json spts;
+    exportSPT(carsSPT, bikeSPT, spts);
+    save("/home/alisot2000/Documents/01_ReposNCode/CSSMALG/code/Simulation/test/data.spt", spts);
+
+	stopMeasureTime();
 
 	startMeasureTime("creating random actors");
 
