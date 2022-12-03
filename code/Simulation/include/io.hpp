28f4344da6fe11aa1f206aebb1ab79bf8f8b331b
AliSot2000 <alisot200@gmail.com>
2022-12-03 18:09:07 +0100

    + Improved Documentation for io files

--- code/Simulation/include/io.hpp
+++ code/Simulation/include/io.hpp
@@ -11,45 +11,50 @@ using nlohmann::json;
 /*
  * Loads a file with json format into a json buffer
  *
- * @param file, path to file
- * @param input json obj or sth to write the content into
+ * @param file: path to file
+ * @param input: json obj or sth to write the content into
  *
  * @returns True <=> the loading was successful.
 */
 bool loadFile(const std::string& file, json& input);
 
 /*
-    Small wrapper to determine if the map contains a precomputed SPT.
+ * Small wrapper to determine if the map contains a precomputed SPT.
+ *
+ * @param map: json object provided via file.
+ *
+ * @returns bool, weather the map contains precomputed SPTs.
  */
 bool hasPrecompute(const json& map);
 
 /*
- * Imports a json file into the c++ data structure.
+ * Imports a json object into the c++ data structure.
  *
- * @param world (world) of current simulation
- * @param map the map loaded from the json file
+ * @param world: (world) of current simulation
+ * @param map: the map loaded from the json file
  *
  * @returns void
  */
 void importMap(world_t& world, nlohmann::json& map);
 
 /*
- * Imports a json file into the c++ data structure.
+ * Imports a json object into the c++ data structure.
  *
- * @param world (world) of current simulation
- * @param agents the agents loaded from the json file
+ * @param world: (world) of current simulation
+ * @param agents: the agents loaded from the json file
  *
  * @returns void
  */
 void importAgents(world_t& world, json& agents, SPT carsSPT, SPT bikeSPT);
 
 /*
- * Docs in progress...
+ * Exports the world to json format. It is the static part of the simulation. The simulation is added step by step with
+ * addFrame.
  *
- * @param world world to export
- * @param time elapsed time
- * @param timeDelta increment steps takein in simulation
- * @param originMap originally imported map
+ * @param world: world to export
+ * @param time: elapsed time
+ * @param timeDelta: increment steps takeing in simulation
+ * @param originMap: originally imported map
  *
  * @returns json marshalling.
  */
@@ -58,24 +63,44 @@ json exportWorld(const world_t& world, const float& time, const float& timeDelta
 /*
  * Adds a frame to the output json.
  *
- * @param world, world to export
- * @param out output json
+ * @param world: world to export
+ * @param out: output json
+ *
  * @returns void
  *
  */
 void addFrame(world_t& world, nlohmann::json& out);
 
 /*
- * Imports a json file into the c++ data structure.
+ * Exports the c++ data structure into a json object.
  *
- * @param world (world) of current simulation
- * @param map the map loaded from the json file
+ * @param world: (world) of current simulation
+ * @param map: the map loaded from the json file
  *
  * @returns void
  */
-
 void save(const std::string& file, const nlohmann::json& out);
 
+/*
+ * Exports the Shortest Path Tree as well as the current world of the simulation to one json file.
+ * The purpose of this function is to save time when loading the map again by not having to compute floyd warshall again.
+ *
+ * @param carTree: Shortest Path Tree for cars
+ * @param bikeTree: Shortest Path Tree for bikes
+ * @param input_world: (world) of current simulation
+ * @param output: json obj or sth to write the content into
+ *
+ * @returns void
+*/
 void exportSPT(const SPT& carTree, const SPT& bikeTree, const json& input_world, json& output);
 
+/*
+ * Imports the Shortest Path Trees from a json object.
+ *
+ * @param carTree: Shortest Path Tree for cars
+ * @param bikeTree: Shortest Path Tree for bikes
+ * @param input: json obj read the content from
+ *
+ * @returns void
+ * */
 void importSPT(SPT& carTree, SPT& bikeTree, const json& input);
