9d3bb4f96653e6eeae57196b33edc81e22e229a8
AliSot2000 <alisot200@gmail.com>
2022-12-05 02:43:22 +0100

    + Added implementations of the int matrix based floyd-warshal algorithm.

---
+++ code/Simulation/include/fastFW.cuh
@@ -0,0 +1,10 @@
+//
+// Created by alisot2000 on 05.12.22.
+//
+
+#ifndef CSSMALG_FASTFW_H
+#define CSSMALG_FASTFW_H
+
+void FloydWarshal(double* dis, int* next, int V);
+
+#endif //CSSMALG_FASTFW_H
