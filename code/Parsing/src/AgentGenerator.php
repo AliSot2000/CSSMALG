a3cb0a3b4147f67573f8f8f4d38be095a6b62998
jh <jh@ivaris.ch>
2022-12-05 20:20:08 +0100

    added bike generation

--- code/Parsing/src/AgentGenerator.php
+++ code/Parsing/src/AgentGenerator.php
@@ -4,12 +4,11 @@ class AgentGenerator
 {
     private array $center;
     private int $radius;
-    private int $carCounter = 0;
 
     private array $coordinates;
     private array $nodesIn = array();
     private array $nodesOut = array();
-    private array $bikePercentages = array(0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2);
+    private array $bikePercentages = array(0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2);
 
     /**
      * constructor generating center of field and the radius
@@ -40,7 +39,7 @@ class AgentGenerator
      * @return void
      */
     private function generateCars(): void {
-        $carDistribution = $this->calculateCarDistribution();
+        $agentAmount = $this->calculateAgentAmount();
         // distribution of cars going into the city center
         // 1/4 of going into city center we assume inter city travel and for 1/4 of rest we assume outer city travel
         $direction = array(0.5, 0.5, 0.5, 0.5, 0.55, 0.65, 0.75, 0.85, 0.8, 0.75, 0.65, 0.5, 0.5, 0.5, 0.5, 0.35, 0.3, 0.2, 0.2, 0.25, 0.35, 0.5, 0.5, 0.5);
@@ -48,88 +47,142 @@ class AgentGenerator
         $numInNodes = count($this->nodesIn) - 1;
         $numOutNodes = count($this->nodesOut) - 1;
 
-        // generate actors that are cars that spawn each minute
-        $carAgents = array();
-        foreach ($carDistribution AS $hour => $amount) {
-            $secondCounter = 0;
-            $amountPerMinute = max(round($amount / 60), 1);
-            $totAmount = 0;
+        // generate agents
+        foreach ($this->bikePercentages AS $bikePercentage) {
+            for ($repetitions = 0; $repetitions < 10; $repetitions++) {
 
-            // number of agents going from out to center (+ center to center (+ center to out (+ out to out)))
-            $directionAmount = array(round($direction[$hour] * $amount * 0.75),  round($direction[$hour] * $amount), round($amount - $amount * (1 - $direction[$hour]) * 0.25));
+                $carAgents = array();
+                $bikeAgents = array();
 
-            for ($i = 0; $i < 60; $i++) {
-                for ($j = 0; $j < $amountPerMinute; $j++) {
-                    // pseudo random length in m
-                    $length = rand(7, 10) / 2;
-                    // pseudo random max velocity in km/h
-                    $maxVelocity = rand(100, 250);
-                    // pseudo random acceleration in m/s^2
-                    $acceleration = rand(150, 500) / 100;
-                    // pseudo random deceleration in m/s^2
-                    $deceleration = rand(200, 600) / 100;
-                    // pseudo random acceleration exponent
-                    $accExp = rand(80, 120) / 10;
+                $secondCounter = 0;
+                $totVehicles = 0;
 
-                    $startId = $endId = 0;
+                foreach ($agentAmount as $hour => $amount) {
+                    $amountPerMinute = max(round($amount / 60), 1);
+                    $amountHour = 0;
 
-                    if ($totAmount < $directionAmount[0]) {
-                        $startId = $this->nodesOut[rand(0, $numOutNodes)];
-                        $endId = $this->nodesIn[rand(0, $numInNodes)];
-                    } else if ($totAmount < $directionAmount[1]) {
-                        while ($startId == $endId) {
-                            $startId = $this->nodesIn[rand(0, $numInNodes)];
-                            $endId = $this->nodesIn[rand(0, $numInNodes)];
+                    // number of agents going from out to center (+ center to center (+ center to out (+ out to out)))
+                    $directionAmount = array(round($direction[$hour] * $amount * 0.75), round($direction[$hour] * $amount), round($amount - $amount * (1 - $direction[$hour]) * 0.25));
+
+                    for ($i = 0; $i < 60; $i++) {
+
+                        // create car agents
+                        for ($j = 0; $j < round($amountPerMinute * (1 - $bikePercentage)); $j++) {
+                            // pseudo random length in m
+                            $length = rand(7, 10) / 2;
+                            // pseudo random max velocity in km/h
+                            $maxVelocity = rand(100, 250);
+                            // pseudo random acceleration in m/s^2
+                            $acceleration = rand(150, 500) / 100;
+                            // pseudo random deceleration in m/s^2
+                            $deceleration = rand(200, 600) / 100;
+                            // pseudo random acceleration exponent
+                            $accExp = rand(80, 120) / 10;
+
+                            $startId = $endId = 0;
+
+                            if ($amountHour < round($directionAmount[0] * (1 - $bikePercentage))) {
+                                $startId = $this->nodesOut[rand(0, $numOutNodes)];
+                                $endId = $this->nodesIn[rand(0, $numInNodes)];
+                            } else if ($amountHour < round($directionAmount[1] * (1 - $bikePercentage))) {
+                                while ($startId == $endId) {
+                                    $startId = $this->nodesIn[rand(0, $numInNodes)];
+                                    $endId = $this->nodesIn[rand(0, $numInNodes)];
+                                }
+                            } else if ($amountHour < round($directionAmount[2] * (1 - $bikePercentage))) {
+                                $startId = $this->nodesIn[rand(0, $numInNodes)];
+                                $endId = $this->nodesOut[rand(0, $numOutNodes)];
+                            } else {
+                                while ($startId == $endId) {
+                                    $startId = $this->nodesOut[rand(0, $numOutNodes)];
+                                    $endId = $this->nodesOut[rand(0, $numOutNodes)];
+                                }
+                            }
+
+                            $carAgents[strval($totVehicles)] = array("start_id" => strval($startId), "end_id" => strval($endId), "length" => $length, "max_velocity" => $maxVelocity, "acceleration" => $acceleration, "deceleration" => $deceleration, "acceleration_exponent" => $accExp, "waiting_period" => $secondCounter);
+                            $amountHour++;
+                            $totVehicles++;
                         }
-                    } else if ($totAmount < $directionAmount[2]) {
-                        $startId = $this->nodesIn[rand(0, $numInNodes)];
-                        $endId = $this->nodesOut[rand(0, $numOutNodes)];
-                    } else {
-                        while ($startId == $endId) {
-                            $startId = $this->nodesOut[rand(0, $numOutNodes)];
-                            $endId = $this->nodesOut[rand(0, $numOutNodes)];
+
+                        // create cyclists
+                        for ($j = 0; $j < round($amountPerMinute * $bikePercentage); $j++) {
+                            // pseudo random length in m
+                            $length = rand(3, 5) / 2;
+                            // pseudo random max velocity in km/h
+                            $maxVelocity = rand(10, 35);
+                            // pseudo random acceleration in m/s^2
+                            $acceleration = rand(50, 150) / 100;
+                            // pseudo random deceleration in m/s^2
+                            $deceleration = rand(100, 300) / 100;
+                            // pseudo random acceleration exponent
+                            $accExp = rand(80, 120) / 10;
+
+                            $startId = $endId = 0;
+
+                            if ($amountHour < round($directionAmount[0] * $bikePercentage)) {
+                                $startId = $this->nodesOut[rand(0, $numOutNodes)];
+                                $endId = $this->nodesIn[rand(0, $numInNodes)];
+                            } else if ($amountHour < round($directionAmount[1] * $bikePercentage)) {
+                                while ($startId == $endId) {
+                                    $startId = $this->nodesIn[rand(0, $numInNodes)];
+                                    $endId = $this->nodesIn[rand(0, $numInNodes)];
+                                }
+                            } else if ($amountHour < round($directionAmount[2] * $bikePercentage)) {
+                                $startId = $this->nodesIn[rand(0, $numInNodes)];
+                                $endId = $this->nodesOut[rand(0, $numOutNodes)];
+                            } else {
+                                while ($startId == $endId) {
+                                    $startId = $this->nodesOut[rand(0, $numOutNodes)];
+                                    $endId = $this->nodesOut[rand(0, $numOutNodes)];
+                                }
+                            }
+
+                            $bikeAgents[strval($totVehicles)] = array("start_id" => strval($startId), "end_id" => strval($endId), "length" => $length, "max_velocity" => $maxVelocity, "acceleration" => $acceleration, "deceleration" => $deceleration, "acceleration_exponent" => $accExp, "waiting_period" => $secondCounter);
+                            $amountHour++;
+                            $totVehicles++;
                         }
+                        $secondCounter += 60;
                     }
-
-                    $carAgents[strval($this->carCounter)] = array("start_id" => strval($startId), "end_id" => strval($endId), "length" => $length, "max_velocity" => $maxVelocity, "acceleration" => $acceleration, "deceleration" => $deceleration, "acceleration_exponent" => $accExp, "waiting_period" => $secondCounter);
-                    $totAmount++;
-                    $this->carCounter++;
                 }
-                $secondCounter += 60;
+
+                $this->writeJSON(array("bikes" => $bikeAgents, "cars" => $carAgents), "sim_$repetitions", strval($bikePercentage * 100) . "_percentBikes");
             }
         }
-
-        $this->writeJSON(array("cars" => $carAgents), "carAgents");
-        unset($carAgents);
     }
 
     /**
-     * calculate how many cars we want to spawn per hour
+     * calculate how many agents we want to spawn per hour
      * @return array
      */
-    private function calculateCarDistribution(): array {
+    private function calculateAgentAmount(): array {
         $area = round($this->distance($this->coordinates["lon1"], $this->coordinates["lat1"], $this->coordinates["lon2"], $this->coordinates["lat1"]) *  $this->distance($this->coordinates["lon1"], $this->coordinates["lat1"], $this->coordinates["lon1"], $this->coordinates["lat2"]));
-        // assumption: at rush hour one new car per 3000m^2 on the whole area of the map per hour
-        $maxCarAmount = $area / 3000;
-        // assumption with at what hour will there be what percentage of $maxCarAmount spawned
+        // assumption: at rush hour one new agent per 4000m^2 on the whole area of the map per hour
+        $maxAgentAmount = $area / 4000;
+        // assumption with at what hour will there be what percentage of $maxAgentAmount spawned
         // increased at morning rush hour, lunch and evening rush hour
-        $carDistribution = array(0.05, 0.05, 0.06, 0.75, 0.1, 0.25, 0.4, 0.65, 0.55, 0.45, 0.4, 0.5, 0.5, 0.4, 0.4, 0.55, 0.8, 1, 0.8, 0.5, 0.3, 0.1, 0.075, 0.05);
+        $agentDistribution = array(0.05, 0.05, 0.06, 0.75, 0.1, 0.25, 0.4, 0.65, 0.55, 0.45, 0.4, 0.5, 0.5, 0.4, 0.4, 0.55, 0.8, 1, 0.8, 0.5, 0.3, 0.1, 0.075, 0.05);
 
-        foreach ($carDistribution AS &$amount) {
-            $amount = round($amount * $maxCarAmount);
+        foreach ($agentDistribution AS &$amount) {
+            $amount = round($amount * $maxAgentAmount);
         }
 
-        return $carDistribution;
+        return $agentDistribution;
     }
 
     /**
      * write the result JSON
      * @param array $data
      * @param string $fileName
+     * @param string $dir
      * @return void
      */
-    private function writeJSON(array $data, string $fileName): void {
-        $handle = fopen("../data/$fileName.json", 'w+');
+    private function writeJSON(array $data, string $fileName, string $dir): void {
+        //create directory if needed
+        if (is_dir("../data/$dir") === false) {
+            mkdir("../data/$dir");
+        }
+
+        $handle = fopen("../data/$dir/$fileName.json", 'w+');
 
         fwrite($handle, json_encode($data));
         fclose($handle);
