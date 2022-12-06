bd3a4b1e66835651e519f47fc2e2d862341864e6
AliSot2000 <alisot200@gmail.com>
2022-12-06 23:08:16 +0100

    + Added new func for very large buffers.

--- code/Simulation/src/base64.cpp
+++ code/Simulation/src/base64.cpp
@@ -56,6 +56,49 @@ std::string base64_encode(BYTE const* buf, unsigned int bufLen) {
     return ret;
 }
 
+void Large_base64_encode(BYTE const* buf, unsigned int bufLen, char* output, unsigned int outputLen) {
+    char* ret = new char[outputLen * 3 + 4];
+//    std::string ret;
+    int i = 0;
+    int j = 0;
+    BYTE char_array_3[3];
+    BYTE char_array_4[4];
+
+    while (bufLen--) {
+        char_array_3[i++] = *(buf++);
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for(i = 0; (i <4) ; i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for(j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (j = 0; (j < i + 1); j++)
+            ret += base64_chars[char_array_4[j]];
+
+        while((i++ < 3))
+            ret += '=';
+    }
+    output = ret;
+    outputLen = 3 * bufLen + 4;
+//    return ret;
+}
+
 std::vector<BYTE> base64_decode(std::string const& encoded_string) {
     int in_len = encoded_string.size();
     int i = 0;
