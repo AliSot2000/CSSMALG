ea634b16c764376877011127a1045f6c54aad777
jh <jh@ivaris.ch>
2022-12-07 21:46:23 +0100

    filtered single roads and added smaller map

--- code/Parsing/src/main.php
+++ code/Parsing/src/main.php
@@ -1,12 +1,13 @@
 <?php
 include_once("Parser.php");
 include_once("AgentGenerator.php");
-
+// small coords: 8.50, 8.55, 47.36, 47.41
+// big coords: 8.50, 8.56, 47.35, 47.43
 // create and run Parser
-$parser = new Parser(8.50, 8.56, 47.35, 47.43);
+$parser = new Parser(8.50, 8.55, 47.36, 47.41);
 $parser->execute();
 
 // create and run AgentGenerator
-$agentGen = new AgentGenerator(8.50, 8.56, 47.35, 47.43);
+$agentGen = new AgentGenerator(8.50, 8.55, 47.36, 47.41);
 $agentGen->execute();
 ?>
