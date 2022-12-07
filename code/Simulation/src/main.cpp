9df5e10dcfb797a7d42fcebc04544522c8312327
AliSot2000 <alisot200@gmail.com>
2022-12-07 12:18:48 +0100

    + Changed function signature

--- code/Simulation/src/main.cpp
+++ code/Simulation/src/main.cpp
@@ -78,9 +78,9 @@ int main(int argc, char* argv[]) {
 
         importAgents(world, agents, carsSPT, bikeSPT);
     } else {
-        world.actors = std::vector<Actor>(randomCars + randomBikes);
-        createRandomActors(world, carsSPT, ActorTypes::Car, 30, 120, world.actors.begin(), world.actors.begin() + randomCars, 4.5f, static_cast<int>(runtime * 0.5));
-        createRandomActors(world, bikeSPT, ActorTypes::Bike, 10, 25, world.actors.begin() + randomCars, world.actors.end(), 1.5f, static_cast<int>(runtime * 0.5));
+        world.actors = std::vector<Actor*>(randomCars + randomBikes);
+        createRandomActors(world, carsSPT, ActorTypes::Car, 30, 120, 0, randomCars, 4.5f, static_cast<int>(runtime * 0.5));
+        createRandomActors(world, bikeSPT, ActorTypes::Bike, 10, 25, randomCars, randomCars + randomBikes, 1.5f, static_cast<int>(runtime * 0.5));
     }
 
 	stopMeasureTime(start);
