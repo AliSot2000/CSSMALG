d6405c3b835fd72144c14f7f70feaf670e0d4a9b
AliSot2000 <alisot200@gmail.com>
2022-12-09 01:23:24 +0100

    + Switched everything in PrecalculateSPT.cpp to pointer functions

--- code/Simulation/src/PrecalculateSPT.cpp
+++ code/Simulation/src/PrecalculateSPT.cpp
@@ -1,9 +1,7 @@
 //
 // Created by alisot2000 on 01.12.22.
 //
-
 #include <iostream>
-#include <string>
 #include <chrono>
 
 #include "actors.hpp"
@@ -25,41 +23,31 @@ int main(int argc, char* argv[]) {
     world_t world;
     nlohmann::json import;
 
-    if (!loadFile(importFile, import)) {
+    if (!loadFile(importFile, &import)) {
         return -1;
     }
 
-    if (hasPrecompute(import)){
+    if (hasPrecompute(&import)){
         std::cout << "File containes a Precomputed SPT. No need to compute it again.";
         return 0;
     }
 
     std::chrono::high_resolution_clock::time_point time = startMeasureTime("importing map");
-    importMap(world, import);
+    importMap(&world, &import);
     stopMeasureTime(time);
 
     time = startMeasureTime("calculating shortest path tree with floyd warshall");
     {
         spt_t carsSPT = calculateShortestPathTree(&world, { StreetTypes::Both, StreetTypes::OnlyCar});
-        std::cout << "Car Tree" << std::endl;
-        for (int i = 0; i < carsSPT.size; i++){
-            for (int j = 0; j < carsSPT.size; j++){
-                std::cout << carsSPT.array[i * carsSPT.size + j] << " ";
-            }
-            std::cout << std::endl;
-        }
-        binDumpSpt(carsSPT, carFile);
+        std::cout << std::endl << "Car Tree" << std::endl;
+        printSPT(&carsSPT);
+        binDumpSpt(&carsSPT, carFile);
     }
     {
         spt_t bikeSPT = calculateShortestPathTree(&world, { StreetTypes::Both, StreetTypes::OnlyBike });
-        binDumpSpt(bikeSPT, bikeFile);
-        std::cout << "Bike Tree" <<std::endl;
-        for (int i = 0; i < bikeSPT.size; i++){
-            for (int j = 0; j < bikeSPT.size; j++){
-                std::cout << bikeSPT.array[i * bikeSPT.size + j] << " ";
-            }
-            std::cout << std::endl;
-        }
+        binDumpSpt(&bikeSPT, bikeFile);
+        std::cout << std::endl << "Bike Tree" <<std::endl;
+        printSPT(&bikeSPT);
     }
     stopMeasureTime(time);
 
