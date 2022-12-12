e35dfb1ff61eb34079605991b59532e811083874
Jannick <jannick.schroer@gmail.com>
2022-12-12 22:51:22 +0100

    + Continued Visualizer

--- code/DataVisualization/visualizer.py
+++ code/DataVisualization/visualizer.py
@@ -26,9 +26,10 @@ class Visualizer:
     def combine_simulations(self, name: str):
         simulations = self.get_simulations(name)
         minutes = 0
+        sim_count = len(simulations)
 
-        if len(simulations) < 1:
-            return
+        if sim_count < 1:
+            raise Exception('No simulations found')
 
         sim_dir = os.path.join(self.path_to_data, name, simulations[0])
 
@@ -38,3 +39,41 @@ class Visualizer:
 
         minutes -= 1
 
+        for simulation in simulations:
+            sim_dir = os.path.join(self.path_to_data, name, simulation)
+
+            time_steps, agent, final = sort_time_steps(os.listdir(sim_dir))
+
+            minute = 1
+            for time_step in time_steps:
+                if not approx_equal(time_step, minute * 60):
+                    raise Exception('Time steps are not equal')
+                with open(os.path.join(sim_dir, time_step)) as f:
+                    data = json.load(f)
+                    # TODO: Sum the timesteps data from the simulations
+
+                minute += 1
+
+            with open(os.path.join(sim_dir, final)) as f:
+                data = json.load(f)
+                # TODO: Sum the of the final timestep from the simulations
+
+        for minute in range(1, minutes):
+            # TODO: Divide the summed data by sim_count
+
+
+def approx_equal(a, b, epsilon: int = 10):
+    return abs(a - b) < epsilon
+
+
+def sort_time_steps(time_steps):
+    new_time_steps = []
+    agent_file_name = 'agents.json'
+    final_file_name = 'final.json'
+    for time_step in time_steps:
+        if time_step == agent_file_name or time_step == final_file_name:
+            continue
+
+        new_time_steps.append(int(time_step.partition('.')[0]))
+    new_time_steps.sort()
+    return new_time_steps, agent_file_name, final_file_name
