245e0ef89271b52d73524c2ba05dae911c1607cc
AliSot2000 <alisot200@gmail.com>
2022-12-13 17:43:57 +0100

    + Counting number of agents that have impossible path and print that number

--- code/Simulation/src/io.cpp
+++ code/Simulation/src/io.cpp
@@ -115,6 +115,7 @@ void importAgents(world_t* world, json* agents, spt_t* carsSPT, spt_t* bikeSPT){
     world->actors = std::vector<Actor*>(agents->at("bikes").size() + agents->at("cars").size());
     std::cout << "importing " << agents->at("bikes").size() << " bikes and " << agents->at("cars").size() << " cars" << std::endl;
     int index = 0;
+    int failed = 0;
 
     // Import Bikes
     for (const auto& [name, data] : agents->at("bikes").items()) {
@@ -148,6 +149,8 @@ void importAgents(world_t* world, json* agents, spt_t* carsSPT, spt_t* bikeSPT){
                     break;
                 }
             }
+        } else {
+            failed++;
         }
 
         world->actors.at(index) = actor;
@@ -185,11 +188,15 @@ void importAgents(world_t* world, json* agents, spt_t* carsSPT, spt_t* bikeSPT){
                     break;
                 }
             }
+        } else {
+            failed++;
         }
 
         world->actors.at(index) = actor;
         index++;
     }
+
+    std::cout << "Found " << failed << " agents with impossible destinations" << std::endl;
 }
 
 json exportWorld(const world_t* world, const float& time, const float& timeDelta, const json* originMap) {
