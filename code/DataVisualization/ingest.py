a86d5885ec03d32d755c5ab8f909663adf207058
Jannick <jannick.schroer@gmail.com>
2022-12-14 22:49:13 +0100

    + Bugfix in Ingest

--- code/DataVisualization/ingest.py
+++ code/DataVisualization/ingest.py
@@ -5,28 +5,35 @@ import os
 
 
 class Ingest:
-
-    def __init__(self, path_to_data: str):
+    def __init__(self, path_to_data: str, name: str):
         self.path_to_data = os.path.normcase(path_to_data)
         self.mongo = mongo.MongoAPI(db_username, db_password, db_ip, db_name)
+        self.name = name
 
-    def get_simulations(self, name: str):
+    def get_simulations(self):
         simulations = []
-        sim_dir = os.path.join(self.path_to_data, name)
+        sim_dir = os.path.join(self.path_to_data, self.name)
         for simulation in sim_dir:
             if os.path.isdir(os.path.join(sim_dir, simulation)):
                 simulations.append(simulation)
 
         return simulations
 
-    def combine_simulations(self, name: str):
-        simulations = self.get_simulations(name)
+    def combine_simulations(self):
+        simulations = self.get_simulations()
 
         if len(simulations) < 1:
             raise Exception('No simulations found')
 
         for simulation in simulations:
-            sim_dir = os.path.join(self.path_to_data, name, simulation)
+            sim_dir = os.path.join(self.path_to_data, self.name, simulation)
+
+            with open(os.path.join(sim_dir, 'agents.json')) as f:
+                data = json.load(f)['setup']['agents']
+
+                for agent in data:
+                    a = data[agent] | {'sim_id': agent}
+                    self.mongo.insert_one('agents', a)
 
             for time_step in os.listdir(sim_dir):
                 if time_step == 'agents.json':
@@ -56,13 +63,13 @@ class Ingest:
                         road_bike_density.append(road['bikeDensity'])
                         road_car_density.append(road['carDensity'])
 
-                self.mongo.insert_one(name, {
-                    'time': time,
-                    'time_step': time_step,
-                    'intersection_bike_flow': intersection_bike_flow,
-                    'intersection_car_flow': intersection_car_flow,
-                    'road_bike_flow': road_bike_flow,
-                    'road_car_flow': road_car_flow,
-                    'road_bike_density': road_bike_density,
-                    'road_car_density': road_car_density
-                })
+                    self.mongo.insert_one(simulation, {
+                        'time': time,
+                        'time_step': time_step,
+                        'intersection_bike_flow': intersection_bike_flow,
+                        'intersection_car_flow': intersection_car_flow,
+                        'road_bike_flow': road_bike_flow,
+                        'road_car_flow': road_car_flow,
+                        'road_bike_density': road_bike_density,
+                        'road_car_density': road_car_density
+                    })
