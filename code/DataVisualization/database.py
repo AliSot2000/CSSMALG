369444febad2db1e06e306ac40cb63603607e2c6
Jannick <jannick.schroer@gmail.com>
2022-12-15 13:50:15 +0100

    + Updated Ingest to new MongoDB Standards + Started Rework of visualizer

--- code/DataVisualization/database.py
+++ code/DataVisualization/database.py
@@ -1,4 +1,3 @@
 db_username = 'py'
 db_password = '12345678'
 db_ip = '127.0.0.1:27017'
-db_name = 'cssmalg'
--- code/DataVisualization/ingest.py
+++ code/DataVisualization/ingest.py
@@ -1,5 +1,5 @@
-import mongo_api as mongo
-from database import db_username, db_password, db_ip, db_name
+from mongo_api import MongoAPI
+from database import db_username, db_password, db_ip
 import json
 import os
 
@@ -7,69 +7,92 @@ import os
 class Ingest:
     def __init__(self, path_to_data: str, name: str):
         self.path_to_data = os.path.normcase(path_to_data)
-        self.mongo = mongo.MongoAPI(db_username, db_password, db_ip, db_name)
+        self.mongo = MongoAPI(db_ip, db_username, db_password)
         self.name = name
 
-    def get_simulations(self):
-        simulations = []
-        sim_dir = os.path.join(self.path_to_data, self.name)
-        for simulation in sim_dir:
-            if os.path.isdir(os.path.join(sim_dir, simulation)):
-                simulations.append(simulation)
-
-        return simulations
-
-    def combine_simulations(self):
-        simulations = self.get_simulations()
+    def ingest_simulations(self):
+        simulations = get_sub_folders(os.path.join(self.path_to_data, self.name))
 
         if len(simulations) < 1:
             raise Exception('No simulations found')
 
         for simulation in simulations:
-            sim_dir = os.path.join(self.path_to_data, self.name, simulation)
+            runs = get_sub_folders(os.path.join(self.path_to_data, self.name, simulation))
 
-            with open(os.path.join(sim_dir, 'agents.json')) as f:
-                data = json.load(f)['setup']['agents']
+            for run in runs:
+                run_dir = os.path.join(self.path_to_data, self.name, simulation, run)
+                print(f'Importing {simulation} {run}')
 
-                for agent in data:
-                    a = data[agent] | {'sim_id': agent}
-                    self.mongo.insert_one('agents', a)
+                agents = []
 
-            for time_step in os.listdir(sim_dir):
-                if time_step == 'agents.json':
-                    continue
-                if time_step == 'final.log':
-                    continue
+                with open(os.path.join(run_dir, 'agents.json')) as f:
+                    data = json.load(f)['setup']['agents']
 
-                intersection_bike_flow = []
-                intersection_car_flow = []
+                    for agent in data:
+                        agents.append(agent)
+                        print(f' - Inserted Agent: {agent}', end='\r')
+                    print(f' - All Agents Inserted')
 
-                road_bike_flow = []
-                road_car_flow = []
-                road_bike_density = []
-                road_car_density = []
+                self.mongo.insert(simulation, f'{run}_agents', agents)
 
-                time = int(time_step.split('.')[0])
+                time_steps = []
 
-                with open(os.path.join(sim_dir, time_step)) as f:
-                    data = json.load(f)
+                for time_step in os.listdir(run_dir):
+                    if time_step == 'agents.json':
+                        continue
+                    if time_step == 'final.json':
+                        continue
 
-                    for intersection in data['intersections']:
-                        intersection_bike_flow.append(intersection['bikeFlow'])
-                        intersection_car_flow.append(intersection['carFlow'])
-                    for road in data['streets']:
-                        road_bike_flow.append(road['bikeFlow'])
-                        road_car_flow.append(road['carFlow'])
-                        road_bike_density.append(road['bikeDensity'])
-                        road_car_density.append(road['carDensity'])
+                    intersection_bike_flow = []
+                    intersection_car_flow = []
 
-                    self.mongo.insert_one(simulation, {
-                        'time': time,
-                        'time_step': time_step,
-                        'intersection_bike_flow': intersection_bike_flow,
-                        'intersection_car_flow': intersection_car_flow,
-                        'road_bike_flow': road_bike_flow,
-                        'road_car_flow': road_car_flow,
-                        'road_bike_density': road_bike_density,
-                        'road_car_density': road_car_density
-                    })
+                    road_bike_flow = []
+                    road_car_flow = []
+                    road_bike_density = []
+                    road_car_density = []
+
+                    time = int(time_step.split('.')[0])
+
+                    with open(os.path.join(run_dir, time_step)) as f:
+                        data = json.load(f)
+
+                        for intersection in data['intersections']:
+                            intersection_bike_flow.append(intersection['bikeFlow'])
+                            intersection_car_flow.append(intersection['carFlow'])
+                        for road in data['streets']:
+                            road_bike_flow.append(road['bikeFlow'])
+                            road_car_flow.append(road['carFlow'])
+                            road_bike_density.append(road['bikeDensity'])
+                            road_car_density.append(road['carDensity'])
+
+                        time_steps.append({
+                            'time': time,
+                            'time_step': time_step,
+                            'intersection_bike_flow': intersection_bike_flow,
+                            'intersection_car_flow': intersection_car_flow,
+                            'road_bike_flow': road_bike_flow,
+                            'road_car_flow': road_car_flow,
+                            'road_bike_density': road_bike_density,
+                            'road_car_density': road_car_density
+                        })
+
+                        print(f' - Inserted Timestep: {time}', end='\r')
+
+                self.mongo.insert(simulation, f'{run}_timesteps', time_steps)
+                print(' - All Timesteps Inserted')
+            print('')
+
+
+def get_sub_folders(path):
+    sub_folders = []
+    for sub_folder in os.listdir(path):
+        if os.path.isdir(os.path.join(path, sub_folder)):
+            sub_folders.append(sub_folder)
+
+    return sub_folders
+
+
+if __name__ == '__main__':
+    i = Ingest('/var/lib/mongodb/', 'LARGE_TRAFFIC_SIG')
+    i.ingest_simulations()
+
