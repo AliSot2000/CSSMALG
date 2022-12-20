1c5713a882db7779c8f170e4e54c55ab138dbcdb
AliSot2000 <alisot200@gmail.com>
2022-12-20 17:16:25 +0100

    + Added documentation

--- code/Simulation/include/fastFW.cuh
+++ code/Simulation/include/fastFW.cuh
@@ -5,6 +5,13 @@
 #ifndef CSSMALG_FASTFW_H
 #define CSSMALG_FASTFW_H
 
+/**
+Compute floyd Warshall algorithm on a graph
+
+@param dis, pointer to a Array of size V*V, where V is the number of vertices in the graph, containing the distance between points.
+@param next, pointer to a Array of size V*V, where V is the number of vertices in the graph, containing the next vertex in the shortest path.
+@param V, number of vertices in the graph
+*/
 void FloydWarshal(double* dis, int* next, int V);
 
 #endif //CSSMALG_FASTFW_H
